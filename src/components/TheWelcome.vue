e04dbaec825bfd815bb6c5e37da20985e7b84e82
laurent <laurent.richir@grow-your-business.fr>
2023-07-26 10:24:10 +0200

    first commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const callSpring = async () => {
+
+  const response = await fetch(import.meta.env.VITE_SPRING_BOOT_URL,{
+    headers:{
+      method:'get'
+    }
+  })
+ if(response.status == '200'){
+   console.log('New User Created')
+ }else{
+   console.log('Error')
+ }
+}
+
+
+</script>
+
+<template>
+  <button @click="callSpring" >test</button>
+</template>
